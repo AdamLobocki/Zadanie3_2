29be622b202e97a9c1729dee1af1512feb6aa6cb
Adam <adamlobocki@gmail.com>
2019-05-09 14:20:04 +0200

    Zadanie 3.2

---
+++ src/Odpowiedzi.java
@@ -0,0 +1,18 @@
+public class Odpowiedzi {
+/*
+    Klasa to szablon pozwalającym definiować  własne, bardziej złożone typy danych.
+    Obiekt to konkretne "wypełnienie danej klasy". Zestawienie powiązanych ze soba danych. Obiekty są tworzone na podstawie klas. Jedna klasa tworzy wiele obiektów
+    Zmienna to konstrukcja programistyczna posiadająca trzy podstawowe atrybuty: symboliczną nazwę, miejsce przechowywania i wartość.
+    Każda zmienna musi mieć określony typ, np. int, albo String.
+    Jeżeli deklarację zmiennej poprzedzimy słowem final, to w dalszej części kodu nie możemy zmienić jej wartości.
+    W klasie mogą się znajdować obiekty, metody i konstruktory
+    metody są wywoływane przez operator konstruktora i służą do tworzenia nowych obiektów
+    Jeśli w klasie zdefiniowano chociaż jeden konstruktor to kompilator nie wygeneruje dla nas konstruktora domyślnego.
+
+            Zakładając, że istnieje klasa Car, to przy zapisie:
+
+    Car car1 = new Car(„BMW”, „M5”, 2007, 2.6);  Car car2 = car1;
+
+    car1 i car2 to dwie referencje wskazujące na ten sam obiekt
+*/
+}
